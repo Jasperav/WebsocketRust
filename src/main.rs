cfb5c30a54b81f47833c9416b2e2eff1143d2822
Jasper <jasperav@hotmail.com>
2019-12-30 15:50:46 +0100

    Added broadcaster

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,56 @@
-use ws::{listen, connect};
+use ws::{listen, connect, Sender, Handler, Message, Handshake};
 use std::thread::{spawn, sleep};
 use std::time::Duration;
+use std::sync::Arc;
+
+struct Client {
+    c: Sender,
+    b: Arc<Sender>
+}
+
+impl Handler for Client {
+    fn on_open(&mut self, _: Handshake) -> Result<(), ws::Error> {
+        self.c.send("") // Send a message to open the connection, weird
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<(), ws::Error> {
+        self.b.shutdown().unwrap();
+        self.c.shutdown().unwrap();
+
+        Ok(())
+    }
+}
 
 fn main() {
-    run_ws();
+    let s = run_ws();
+    let arc = Arc::new(s);
 
     connect("ws://127.0.0.1:3042", |c| {
-        c.send(""); // Don't know why something needs to be send to open up the connection
-        sleep(Duration::from_secs(1));
-        move |m| {
-            c.shutdown().unwrap();
-            Ok(())
+        Client {
+            c,
+            b: Arc::clone(&arc)
         }
     }).unwrap();
 
     run_ws();
 }
 
-fn run_ws() {
-    let _ = spawn(|| {
-        listen("127.0.0.1:3042", |c| {
-            move |msg| {
-                c.send(""); // Send something back
-                // Not sure why I can not shutdown the websocket on the instance of the websocket itself.
-                Ok(())
-            }
-        }).unwrap();
+fn run_ws() -> Sender {
+    let socket = ws::Builder::new().build(|c: ws::Sender| {
+        move |msg| {
+            c.send(""); // Send something back
+            // Not sure why I can not shutdown the websocket on the instance of the websocket itself.
+            Ok(())
+        }
+    }).unwrap();
+
+    let broadcast = socket.broadcaster();
+
+    let _ = spawn(move || {
+        socket.listen("127.0.0.1:3042").unwrap();
     });
 
     sleep(Duration::from_secs(1)); // Let server startup
+
+    broadcast
 }
